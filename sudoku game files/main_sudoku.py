7959dd3c7e465219a2205e0613e74b23ecd61bbd
Emma <emmalobley3000@hotmail.co.uk>
2023-12-07 09:52:10 +0000

    updated class implementation to main.py

--- sudoku game files/main_sudoku.py
+++ sudoku game files/main_sudoku.py
@@ -2,19 +2,50 @@ import requests
 # import pandas as pd
 # from pprint import pprint as pp
 
-asciiBoard = "\=======+=======+=======/\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "+-------+-------+-------+\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "+-------+-------+-------+\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "| . . . | . . . | . . . |\n" \
-             "/=======+=======+=======\ "
+
+class SudokuBoard:
+    def __init__(self, board):
+        self.board = board
+
+    def format_board(self):
+        ascii_board = "\=======+=======+=======/\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "+-------+-------+-------+\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "+-------+-------+-------+\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "| . . . | . . . | . . . |\n" \
+          "/=======+=======+=======\ "
+
+        single_list = [num for slist in self.board for num in slist]
+
+        # put sudoku list into a condensed list
+        result = []
+        index = 0
+
+        # check if the character in the board is a '.'
+        for char in ascii_board:
+            if char == '.':
+                # check if the number is 0 / replace with dot if so
+                if single_list[index] == 0:
+                    result.append('.')
+                else:  # put numbers into asciiBoard
+                    result.append(str(single_list[index]))
+                index += 1
+            else:
+                result.append(char)
+
+        new_ascii = ''.join(result)
+        return new_ascii
+
+    def get_board(self):
+        return self.board
+
 
 # boolean function, validate if sudoku is completed
 # update check method - cannot compare to solution grid with this api
@@ -29,17 +60,20 @@ def get_difficulty():
         difficulty = input("Invalid choice. Please select difficulty: ").lower()
     return difficulty
 
+
 # check user choice of number valid
 def valid_difficulty(difficulty):
     return difficulty in {"easy", "medium", "hard"}
 
+
 # loop until you get a board of your chosen difficulty
 # look into more efficient way of doing this if time allows
-def get_sudoku(difficulty):
+def get_new_sudoku(difficulty):
     endpoint = 'https://sudoku-game-and-api.netlify.app/api/sudoku'
     response = requests.get(endpoint)
     sudoku = response.json()
     return sudoku[difficulty]
+
 
 def get_user_move():
     user_row = int(input(" Please select row (1-9): "))
@@ -53,61 +87,32 @@ def get_user_move():
     while not valid_number(user_num):
         user_num = int(input(" Invalid choice of number, please try again: "))
 
-    return (user_row, user_col, user_num)
+    return user_row - 1, user_col - 1, user_num
+
 
 # check user choice of number valid
 def valid_number(number):
     return number in {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+
 # ~~~~~~~~~~~ main game loop ~~~~~~~~~~~~~~~~~~
 
 def main():
     difficulty = get_difficulty()
-    board = get_sudoku(difficulty)
+    board = get_new_sudoku(difficulty)
+    new_board = SudokuBoard(board)
+
     print("     Welcome to sudoku! ")
     print(f'    Difficulty = {difficulty.title()}')
     # pp(board)
 
     # cannot check against sol board so using i as placeholder for now
     i = 0
-    while i < 10: # not completed_sudoku(board, solution):
-
-        single_list = [num for slist in board for num in slist]
-
-        # put sudoku list into a condensed list
-        result = []
-        index = 0
-
-        # check if the character in the board is a '.'
-        for char in asciiBoard:
-            if char == '.':
-                # check if the number is 0 / replace with dot if so
-                if single_list[index] == 0:
-                    result.append('.')
-                else: # put numbers into asciiBoard
-                    result.append(str(single_list[index]))
-                index += 1
-            else:
-                result.append(char)
-
-        asciiB_string = ''.join(result)
-        print(asciiB_string)
-
+    while i < 10:  # not completed_sudoku(board, solution):
+        print(new_board.format_board())
         user_move = get_user_move()
-        user_row = user_move[0] - 1
-        user_col = user_move[1] - 1
-        user_num = user_move[2]
+        new_board.get_board()[user_move[0]][user_move[1]] = user_move[2]
 
-        board[user_row][user_col] = user_num
-
-        # if solution[user_row][user_col] == user_num:
-        #     print("         Correct!")
-        #     board[user_row][user_col] = user_num
-        #
-        # else:
-        #     print(" Incorrect. Try again.")
-
-        # pp(board)
 
 if __name__ == "__main__":
     while True:
